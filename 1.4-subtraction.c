c13857729b9227b932b558bf5ca736bb92f70e4d
John Behan <behanj@gmail.com>
2017-04-17 12:06:18 +0700

    exercise 1.4

---
+++ 1.4-subtraction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int val1, val2, ans;
+
+  val1 = 15;
+  val2 = 87;
+  ans = val2 - val1;
+
+  printf ("Subtracting %i from %i gives you %i\n", val1, val2, ans);
+
+  return 0;
+}
